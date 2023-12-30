11e24a6fb0ac479f1a67c1a5f8ce64dbd26d8fa1
Zsolt Nagy <info@zsoltnagy.eu>
2023-12-30 17:00:04 +0100

    markup + target generálása

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,20 +1,11 @@
-/*
-    Számítógép gondol egy egész számra 1 és 100 között. A felhasználó egy input
-    mezőbe írja a tippjeit. A számítógép minden tippre a következők egyikét
-    válaszolja: 
+let target;
 
-    - a keresett szám kisebb 
-    - a keresett szám nagyobb 
-    - gratulálok, eltaláltad!
+function generateTarget() {
+    return Math.floor(Math.random() * 100) + 1;
+}
 
-    Amint eltaláltad a keresett számot, a játék véget ér. A játék végén kiírjuk
-    a tippek számát.
+function startGame() {
+    target = generateTarget();
+}
 
-    Bármikor lehet új játékot indítani. Ilyenkor a korábbi tippek törlődnek és
-    a számítógép új számot generál. 
-
-    A játék során a tippek listája időrendi sorrendben megjelenik. Felül
-    található a legutolsó tipped, alul pedig az első tipped.
-
-    
-*/
+startGame();
