00f0c1228923e91980f85ad8a1a6052c826d54ed
Zsolt Nagy <info@zsoltnagy.eu>
2023-12-30 17:07:19 +0100

    reset game

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,11 +1,24 @@
+let guessListContainer = document.querySelector(".js-guess-container");
+let currentGuessField = document.querySelector(".js-guess-field");
+let newGameButton = document.querySelector(".js-new-game-button");
+
 let target;
 
 function generateTarget() {
     return Math.floor(Math.random() * 100) + 1;
 }
 
+function resetGame() {
+    // korábbi tippek törlése
+    guessListContainer.innerHTML = "";
+    // input mező tartalmának törlése
+    currentGuessField.value = "";
+}
+
 function startGame() {
+    resetGame();
     target = generateTarget();
 }
 
 startGame();
+newGameButton.addEventListener("click", startGame);
